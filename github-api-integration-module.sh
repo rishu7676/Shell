b593c81a436d3b595145d10f7b8ab8097430977c
rishu7676 <therishunrishabh@gmail.com>
2026-09-06 00:16:13 +0530

    chore: updated desc

--- github-api-integration-module.sh
+++ github-api-integration-module.sh
@@ -4,7 +4,7 @@
 # Version: v1
 #
 #editing it through notepad
-#
+#new edit
 # This script will help users to communicate and retrieve information from GitHub
 # Usage:
 #   Please provide your github token and rest api to the script as input
